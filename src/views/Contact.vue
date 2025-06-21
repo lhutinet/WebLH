a1859e284a8ffe3e186511aeb9a3aea379e00cef
lhutinet <lhutinet@free.fr>
2025-06-21 12:25:47 +0200

    002

---
+++ src/views/Contact.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>contact</h1>
+    <p>Laurent Hutinet</p>
+    <p>3 grande rue </p>
+    <p>38150 Roussilon</p>
+    <p>tel: <a href="tel:+33662316823">0662316823</a></p>
+    <p>mail :<a href="mailto:lhutinet">lhutinet@free.fr</a></p>
+</template>
+<script setup>
+
+</script>
