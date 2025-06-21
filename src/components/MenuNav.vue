63ff42806537dcd14935fcbdc08ff2939d2e4e46
lhutinet <lhutinet@free.fr>
2025-06-21 14:00:03 +0200

    003-1

--- src/components/MenuNav.vue
+++ src/components/MenuNav.vue
@@ -3,8 +3,6 @@
     <nav>
         <ul>
             <li><router-link to="/">Accueil</router-link></li>
-            <li><router-link to="/apropos">À propos</router-link></li>
-            <li><router-link to="/contact">Contact</router-link></li>
             <li><router-link to="/AmHTML">aide HTML</router-link></li>
             <li><router-link to="/AmCss">aide css</router-link></li>
             <li><router-link to="/Amscss">aide scss</router-link></li>
@@ -12,6 +10,8 @@
             <li><router-link to="/Amvue">aide vue</router-link></li>
             <li><router-link to="/Amreact">aide react</router-link></li>
             <li><router-link to="/Amgithub">aide git hub</router-link></li>
+            <li><router-link to="/contact">Contact</router-link></li>
+            <li><router-link to="/apropos">À propos</router-link></li>
         </ul>
     </nav>
 
