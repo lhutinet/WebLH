2dfb19a387a331e07f3777ffcfd33209f190d865
lhutinet <lhutinet@free.fr>
2025-06-24 23:14:19 +0200

    011.1

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -5,15 +5,18 @@
 
 
             <div class="coordonneesContent">
-                <form @submit.prevent="submitForm" class="contactForm" name="contact" netlify>
-                    <input type="text" v-model="form.nom" name="nom" placeholder="Votre nom" required />
-                    <input type="text" v-model="form.prenom" name="prenom" placeholder="Votre prénom" required />
-                    <input type="email" v-model="form.email" name="email" placeholder="Votre email" required />
-                    <input type="tel" v-model="form.telephone" name="telephone" placeholder="Votre téléphone"
-                        required />
-                    <textarea v-model="form.demande" name="demande" placeholder="Votre demande" required></textarea>
+                <form name="contact" method="POST" data-netlify="true" netlify>
+                    <input type="hidden" name="form-name" value="contact" />
+
+                    <input type="text" name="nom" placeholder="Votre nom" required />
+                    <input type="text" name="prenom" placeholder="Votre prénom" required />
+                    <input type="email" name="email" placeholder="Votre email" required />
+                    <input type="tel" name="telephone" placeholder="Votre téléphone" required />
+                    <textarea name="demande" placeholder="Votre demande" required></textarea>
+
                     <button type="submit">Envoyer</button>
                 </form>
+
                 <p>Laurent Hutinet</p>
                 <p>3 grande rue</p>
                 <p>38150 Roussillon</p>
