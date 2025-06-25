de820b4be7d9cffb3e3e2524a165cc7b31ba707d
lhutinet <lhutinet@free.fr>
2025-06-25 13:04:16 +0200

    11.8

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -26,7 +26,7 @@
                 <p>
                     Mail : <a href="mailto:lhutinet@free.fr">lhutinet@free.fr</a>
                 </p>
-                <p>11.7</p>
+                <p>11.8</p>
             </div>
 
             <iframe
