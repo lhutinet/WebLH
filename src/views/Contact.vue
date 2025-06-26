10d7cead171a862bab77d437dad8711a609542ae
lhutinet <lhutinet@free.fr>
2025-06-26 11:42:32 +0200

    014- contact css

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,29 +1,43 @@
 <template>
     <div class="contactContainer">
-        <h1>Contact</h1>
+        <h2>Contact</h2>
+        <h3>Laurent Hutinet</h3>
+        <div class="adressContent">
+            <div class="coordContent">
+                <div class="logoCoo"><img src="/public/appel.png" alt="telephone"></div>
+                <p>
+                    Tél : <a href="tel:+33662316823">06 62 31 68 23</a>
+                </p>
+            </div>
+
+            <div class="coordContent">
+                <div class="logoCoo"><img src="/public/e-mail.png" alt="mail"></div>
+                <p>
+                    Mail : <a href="mailto:lhutinet@free.fr">lhutinet@free.fr</a>
+                </p>
+            </div>
+            <div class="coordContent">
+                <div class="logoCoo"><img src="/public/adresse-du-domicile.png" alt="adresse postale"></div>
+                <p>3 grande rue</p>
+                <p>38150 Roussillon</p>
+            </div>
+
+        </div>
         <div class="contactContent">
 
 
             <div class="coordonneesContent">
                 <form name="contact" method="POST" data-netlify="true" @submit.prevent="submitForm">
                     <input type="hidden" name="form-name" value="contact" />
-                    <input type="text" name="nom" placeholder="Votre nom" required />
-                    <input type="text" name="prenom" placeholder="Votre prénom" required />
-                    <input type="email" name="email" placeholder="Votre email" required />
-                    <input type="tel" name="telephone" placeholder="Votre téléphone" required />
+                    <input type="text" name="nom" placeholder=" nom" required />
+                    <input type="text" name="prenom" placeholder=" prénom" required />
+                    <input type="email" name="email" placeholder=" email" required />
+                    <input type="tel" name="telephone" placeholder=" téléphone" required />
                     <textarea name="demande" placeholder="Votre demande" required></textarea>
                     <button type="submit">Envoyer</button>
                 </form>
 
-                <p>Laurent Hutinet</p>
-                <p>3 grande rue</p>
-                <p>38150 Roussillon</p>
-                <p>
-                    Tél : <a href="tel:+33662316823">06 62 31 68 23</a>
-                </p>
-                <p>
-                    Mail : <a href="mailto:lhutinet@free.fr">lhutinet@free.fr</a>
-                </p>
+
 
             </div>
             <div class="map-wrapper">
